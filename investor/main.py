2dc5e8d1500086787fcdd9be6fb093c31634d199
afrolov <frolov.public@gmail.com>
2020-10-28 15:42:22 +0300

    Calculate speculative strategy

--- investor/main.py
+++ investor/main.py
@@ -47,13 +47,18 @@ class Fund:
         assert investment == 0
         return self.estimate()
 
-    def buy_assets(self, investment: float, month_idx: int):
-        commission_threshold = max(
-            ct for ct in self._params.commission_rates if ct <= investment
-        )
-        commission_rate = self._params.commission_rates[commission_threshold]
-        commission = investment * commission_rate
-        investment -= commission
+    def buy_assets(self,
+                   investment: float,
+                   month_idx: int,
+                   with_commission: bool = True):
+        if with_commission:
+            commission_category = max(
+                cc for cc in self._params.commission_rates if cc <= investment
+            )
+            commission_rate = self._params.commission_rates[commission_category]
+            commission = investment * commission_rate
+            investment -= commission
+
         price = self.price_history[month_idx]
         assets = investment / price
         self._assets += assets
@@ -65,20 +70,26 @@ class Fund:
 
 
 class Investor:
-    def __init__(self,
-                 params: config.Params,
-                 stable_fund: Fund,
-                 volatile_fund: Fund):
+    def __init__(self, params: config.Params):
         self._params = params
-        self._stable_fund = stable_fund
-        self._volatile_fund = volatile_fund
+        self._stable_fund = Fund(params, prices.BONDS_HISTORY)
+        self._volatile_fund = Fund(params, prices.SHARES_HISTORY)
 
-    def invest(self) -> float:
+    def invest(self, from_stable: bool = False) -> float:
         cheaper_months = self._volatile_fund.find_cheaper_months()
         for i in range(prices.HISTORY_LENGTH):
-            fund = self._volatile_fund if i in cheaper_months \
-                else self._stable_fund
+            if i not in cheaper_months:
+                self._stable_fund.buy_assets(self._params.monthly_investment, i)
+                continue
+
+            fund = self._volatile_fund
             fund.buy_assets(self._params.monthly_investment, i)
+            if not from_stable:
+                continue
+
+            investment = self._stable_fund.estimate()
+            self._stable_fund = Fund(self._params, prices.BONDS_HISTORY)
+            fund.buy_assets(investment, i, with_commission=False)
 
         return self.estimate()
 
@@ -112,17 +123,18 @@ def main():
     params = config.Params()
     report = Report(params)
 
-    bonds_fund = Fund(params, prices.BONDS_HISTORY)
-    shares_fund = Fund(params, prices.SHARES_HISTORY)
-    value = Investor(params, bonds_fund, shares_fund).invest()
+    value = Investor(params).invest()
     report.add('multi-fund', value)
+
+    value = Investor(params).invest(from_stable=True)
+    report.add('speculative', value)
 
     price_histories = {'bonds': prices.BONDS_HISTORY,
                        'shares': prices.SHARES_HISTORY}
     for asset_type, price_history in price_histories.items():
         value = Fund(params, price_history).invest_if_cheaper()
         report.add(f'cheaper {asset_type}', value)
-        for i in (1, 2, 3, 4, 5, 6):
+        for i in (1, 3, 5):
             value = Fund(params, price_history).invest_periodically(i)
             report.add(f'{i}-months {asset_type}', value)
 
