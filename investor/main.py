01b1a2f3c5909497cd04d24cdb4fb9c8ca44d6a2
afrolov <frolov.public@gmail.com>
2020-10-28 13:42:36 +0300

    Prices history refactoring

--- investor/main.py
+++ investor/main.py
@@ -1,3 +1,4 @@
+import prices
 import typing
 
 COMMISSION_RATES = {
@@ -8,88 +9,6 @@ COMMISSION_RATES = {
 INTEREST_RATE = 0.035
 MONTHLY_INVESTMENT = 100_000
 MONTHS_IN_YEAR = 12
-
-# "Облигации Плюс" from 2017-10-23 to 2020-10-23
-BONDS_PRICE_HISTORY = (
-    3741.74,
-    3767.22,
-    3787.6,
-    3861.09,
-    3931.06,
-    3959.46,
-    3921.55,
-    3953.64,
-    3949.79,
-    3964.15,
-    3863.9,
-    3875.97,
-    3907.98,
-    3910.74,
-    3925.15,
-    4002.74,
-    4025.65,
-    4063.61,
-    4093.82,
-    4140.52,
-    4209.36,
-    4247.81,
-    4285.52,
-    4354.63,
-    4420.06,
-    4452.62,
-    4488.55,
-    4560.36,
-    4604.7,
-    4363,
-    4562.63,
-    4655.02,
-    4695.89,
-    4718.28,
-    4711.1,
-    4747.56,
-    4774.88
-)
-
-# "Ликвидные акции" from 2017-10-23 to 2020-10-23
-SHARES_PRICE_HISTORY = (
-    3859.41,
-    4064.93,
-    3942.53,
-    4374.1,
-    4481.34,
-    4412.38,
-    4391.72,
-    4547.28,
-    4515.96,
-    4648.97,
-    4656.81,
-    4964.05,
-    4908.94,
-    4886.37,
-    4899.49,
-    5196.63,
-    5141.94,
-    5131.54,
-    5309.35,
-    5343.91,
-    5710.68,
-    5748.18,
-    5688.8,
-    5841.26,
-    5957.72,
-    6205.33,
-    6345.63,
-    6578.52,
-    6592.28,
-    4851.45,
-    5498.12,
-    5615.63,
-    5893.07,
-    6149.01,
-    6345.99,
-    6186.85,
-    6158.35
-)
 
 
 class Fund:
@@ -188,13 +107,12 @@ class Investor:
 
 
 def main():
-    bonds_fund = Fund(BONDS_PRICE_HISTORY)
-    shares_fund = Fund(SHARES_PRICE_HISTORY)
+    bonds_fund = Fund(prices.BONDS_HISTORY)
+    shares_fund = Fund(prices.SHARES_HISTORY)
     investor = Investor(bonds_fund, shares_fund)
     investor.invest()
     print(f'multi-fund strategy: {investor}')
-    for price_history in (BONDS_PRICE_HISTORY,
-                          SHARES_PRICE_HISTORY):
+    for price_history in (prices.BONDS_HISTORY, prices.SHARES_HISTORY):
         print('#' * 100)
         fund = Fund(price_history)
         fund.invest_if_cheaper()
